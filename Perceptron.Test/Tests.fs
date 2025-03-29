88f9ff10e7ba78be98f3063d15741b1c861e8206
Chris Bremer <cbremer4@gmail.com>
2025-03-28 23:24:51 -0500

    Added backpropagation

--- Perceptron.Test/Tests.fs
+++ Perceptron.Test/Tests.fs
@@ -23,3 +23,24 @@ let ``Can GBM``() =
     let data = gbm.forward(tt{yield[1]},tt{yield[1]},tt{yield[100]})
     let series = data[torch.TensorIndex.Ellipsis,zeroIndex,zeroIndex,zeroIndex]
     series.ToString(TensorStringStyle.Julia) |> Assert.FailWith "%s"
+    
+    
+[<Fact>]
+let ``Forward``() =
+    use loss = new ShortTermLoss(torch.tensor(0.01f))
+    let model = Model.model 5
+    let optimizer = Model.grad 0.01 model
+    let observed = torch.tensor([|1.0f;2.0f;3.0f;4.0f;5.0f|])
+    model.zero_grad()
+    let result = loss.forward(observed)
+    let input = torch.tensor([|1.0f;2.0f;3.0f;4.0f;5.0f|])
+    model.call(input).softmax(0).dot(result).backward()
+    optimizer.step()
+    
+    
+    
+    
+    
+
+
+
--- Perceptron/Model.fs
+++ Perceptron/Model.fs
@@ -1,20 +1,28 @@
 namespace Perceptron
+open System.Collections.Generic
 open TorchSharp
 open type TorchSharp.TensorExtensionMethods
 
 
 
-type ShortTermLoss(interestRate : float ) =
-    inherit torch.nn.Module<torch.Tensor,torch.Tensor,torch.Tensor>("ShortTermLoss")
-    override _.forward (input: torch.Tensor, target: torch.Tensor) =
-        let diffs = torch.roll(input,1) / input
-        diffs[0].set_(0.0) |> ignore
-        let exps = torch.pow(interestRate, torch.arange(0, diffs.size(0), 1, dtype = torch.float))
-        torch.sum(diffs * exps)
+type ShortTermLoss(termInterestRate : torch.Tensor) =
+    inherit torch.nn.Module<torch.Tensor,torch.Tensor>("ShortTermLoss")
+    override _.forward (observed) =
+        let rates = torch.unsqueeze(termInterestRate, 0)
+        let observed = torch.log(observed.flatten())        
+        let n = observed.size(0)
+        let logDiscount = torch.log(1 / (1 + rates)) * torch.arange(0, n, 1, dtype = torch.float)        
+        let sell = (observed - observed[0]*torch.ones(n) + logDiscount).mean()
+        let buy = -sell
+        let hold = torch.zeros(sell.size())
+        torch.stack(List([buy;hold;sell;]))
+        
+        
+        
         
 
 module Model =
-    let model n_in : torch.nn.Module = torch.nn.Linear(n_in, 2)    
+    let model n_in : torch.nn.Module<torch.Tensor,torch.Tensor> = torch.nn.Linear(n_in, 3, dtype=torch.float)
     let grad learning_rate (model: torch.nn.Module) =  torch.optim.Adam(model.parameters(), learning_rate)
     
     
