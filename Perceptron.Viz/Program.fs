492706cf70fe345134c48e8a1a5ac0848cf12284
Chris Bremer <cbremer4@gmail.com>
2025-04-06 16:54:00 -0500

    adding .node-version, node artifacts, updated .gitignore

---
+++ Perceptron.Viz/Program.fs
@@ -0,0 +1,16 @@
+﻿open Browser
+open Fable.Core
+open Fable.Core.JsInterop
+
+[<Emit("undefined")>]
+let undefined: obj = jsNative
+
+[<EntryPoint>]
+let main args =
+    console.log(undefined)
+    let div = document.createElement "div"
+    div.innerHTML <- """
+        <h1>It's a bye</h1>   
+    """
+    document.body.appendChild div |> ignore
+    0
